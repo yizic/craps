2e5f060d5126d2707d04e82320d4e6546d09be5c
yizic <yizick@gmail.com>
2020-03-25 17:48:09 +0100

    commit premiere fenetre

--- CrapsTk.py
+++ CrapsTk.py
@@ -1,17 +1,68 @@
-# coding: utf-8
- 
-from tkinter import * 
+from tkinter import *
 from random import *
+
+#1er ecran
+
+def debut():
+
+    imphrase = canimgphrase.create_image(340, 55, image=imagePhrase)
+    canimgphrase.grid(row=0, columnspan = 4)
+    imregle= canregle.create_image(165, 118, image= regle)
+    canregle.grid(row=3, column =0, columnspan = 2)
+    image_des= canmorpion.create_image(165, 118, image= png_des)
+    canmorpion.grid(row=3, column =2, columnspan = 2)
+
+    question.grid(row=1, column=1, columnspan=2)
+    bouton_IA.grid(row=2, column=1)
+    bouton_ami.grid(row=2, column=2)
+
+def voir_doc():
+
+    print("Document")
+
+def jouer():
+    #quand tu joue seul
+    print("jouer")
+
+#crétion fenetre
 
 fenetre = Tk()
 
-# frame 1
-FrameLancement = Frame(fenetre, borderwidth=3, relief=GROOVE)
-FrameLancement.pack(side=LEFT, padx=30, pady=30)
+#taille fenetre + couleur
 
-Label(FrameLancement, text="Bonjour et bienvenu ! Prenez place.").pack(padx=10, pady=10)
+fenetre.geometry("663x450")
+fenetre.configure(background="white")
 
-Button(fenetre, text ='Prendre Place').pack(side=TOP, padx=5, pady=3)
-Button(fenetre, text ='Accès aux documents').pack(side=BOTTOM, padx=5, pady=3)
+#positionner la fenetre au mileu et non pas dans un coin de lécran(Vive les tuto)
+
+windowWidth = fenetre.winfo_reqwidth()
+windowHeight = fenetre.winfo_reqheight()
+positionRight = int(fenetre.winfo_screenwidth()/3 - windowWidth/2)
+positionDown = int(fenetre.winfo_screenheight()/3 - windowHeight/2)
+fenetre.geometry("+{}+{}".format(positionRight, positionDown))
+
+#truc a utiliser et detruire (ou remove)
+
+imagePhrase = PhotoImage(file="image/leJeuDuMorpion.png")
+regle = PhotoImage(file="image/regle.png")
+png_des = PhotoImage(file="image/des.png")
+
+question = Label(fenetre, text = "Souhaitez vous jouer contre une IA ou contre un ami", background = "white")
+bouton_IA = Button(fenetre, text = "Contre une IA", command = jouer)
+bouton_ami = Button(fenetre, text = "Contre un ami", command = voir_doc)
+
+canimgphrase = Canvas(fenetre, width=660, height=100, background ="white", highlightthickness =  0)
+canregle = Canvas(fenetre, width=330, height= 237, background ="white", highlightthickness =  0)
+canmorpion = Canvas(fenetre, width=330, height= 237, background ="white", highlightthickness =  0)
+
+bouton_quit = Button(fenetre, text="quitter", command= fenetre.destroy)
+
+debut()
+
+#titre fenetre
+
+fenetre.title("MORPION")
+
+#fini
 
 fenetre.mainloop()
