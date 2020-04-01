2493321c6ac938816db5d34d70c1bcbd5899884d
yizic <yizick@gmail.com>
2020-04-01 15:27:57 +0200

    il restait un conflit

--- CrapsTk.py
+++ CrapsTk.py
@@ -54,15 +54,9 @@ imagePhrase = PhotoImage(file="image/jeuDuCraps.png")
 regle = PhotoImage(file="image/regle.png")
 png_des = PhotoImage(file="image/des.png")
 
-<<<<<<< HEAD
 question = Label(fenetre, text = "Souhaitez vous jouer ou voir les documents", background = "white")
 bouton_jouer = Button(fenetre, text = "jouer", command = jouer)
 bouton_docs = Button(fenetre, text = "voir les documents", command = voir_doc)
-=======
-question = Label(fenetre, text = "Souhaitez vous jouer ou voir les documents?", background = "white")
-bouton_IA = Button(fenetre, text = "Jouer", command = jouer)
-bouton_ami = Button(fenetre, text = "Consulter les documents", command = File.voir_doc)
->>>>>>> master
 
 canimgphrase = Canvas(fenetre, width=660, height=100, background ="white", highlightthickness =  0)
 canregle = Canvas(fenetre, width=420, height= 237, background ="white", highlightthickness =  0)
