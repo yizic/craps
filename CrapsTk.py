020a85dbce7b7e095bc0dc684df7e43e13685f27
yizic <yizick@gmail.com>
2020-04-04 12:48:53 +0200

    ouverture correcte de la fenetre de jeu avec fermeture de la première fenetre et affichage portefuille et Entry

--- CrapsTk.py
+++ CrapsTk.py
@@ -10,6 +10,9 @@ from Jeu import *
 class CrapsTk:
 
     def __init__(self):
+
+        self.fenetre = Tk()
+
         self.imphrase = None
         self.canimgphrase = None
         self.imregle = None
@@ -36,25 +39,26 @@ class CrapsTk:
         self.bouton_jouer.grid(row=2, column=1)
         self.bouton_docs.grid(row=2, column=2)
 
+    def voir_jeu(self):
+        
+        self.fenetre.destroy()
+        craps.voir_jeu()
+
     #crétion fenetre
-    def creation_premiere_fenetre():
-
-        Crapy = CrapsTk()
-
-        fenetre = Tk()
+    def creation_premiere_fenetre(self):
 
         #taille fenetre + couleur
 
-        fenetre.geometry("820x450")
-        fenetre.configure(background="white")
+        self.fenetre.geometry("820x450")
+        self.fenetre.configure(background="white")
 
         #positionner la fenetre au mileu et non pas dans un coin de lécran(Vive les tuto)
 
-        windowWidth = fenetre.winfo_reqwidth()
-        windowHeight = fenetre.winfo_reqheight()
-        positionRight = int(fenetre.winfo_screenwidth()/3 - windowWidth/2)
-        positionDown = int(fenetre.winfo_screenheight()/3 - windowHeight/2)
-        fenetre.geometry("+{}+{}".format(positionRight, positionDown))
+        windowWidth = self.fenetre.winfo_reqwidth()
+        windowHeight = self.fenetre.winfo_reqheight()
+        positionRight = int(self.fenetre.winfo_screenwidth()/3 - windowWidth/2)
+        positionDown = int(self.fenetre.winfo_screenheight()/3 - windowHeight/2)
+        self.fenetre.geometry("+{}+{}".format(positionRight, positionDown))
 
         #truc a utiliser et detruire (ou remove)
 
@@ -62,15 +66,15 @@ class CrapsTk:
         Crapy.regle = PhotoImage(file="image/regle.png")
         Crapy.png_des = PhotoImage(file="image/des.png")
 
-        Crapy.question = Label(fenetre, text = "Souhaitez vous jouer ou voir les documents", background = "white")
-        Crapy.bouton_jouer = Button(fenetre, text = "jouer", command = craps.voir_jeu)
-        Crapy.bouton_docs = Button(fenetre, text = "voir les documents", command = File.voir_doc)
+        Crapy.question = Label(self.fenetre, text = "Souhaitez vous jouer ou voir les documents", background = "white")
+        Crapy.bouton_jouer = Button(self.fenetre, text = "jouer", command = lambda : Crapy.voir_jeu())
+        Crapy.bouton_docs = Button(self.fenetre, text = "voir les documents", command = File.voir_doc)
 
-        Crapy.canimgphrase = Canvas(fenetre, width=660, height=100, background ="white", highlightthickness =  0)
-        Crapy.canregle = Canvas(fenetre, width=420, height= 237, background ="white", highlightthickness =  0)
-        Crapy.cancraps = Canvas(fenetre, width=330, height= 237, background ="white", highlightthickness =  0)
+        Crapy.canimgphrase = Canvas(self.fenetre, width=660, height=100, background ="white", highlightthickness =  0)
+        Crapy.canregle = Canvas(self.fenetre, width=420, height= 237, background ="white", highlightthickness =  0)
+        Crapy.cancraps = Canvas(self.fenetre, width=330, height= 237, background ="white", highlightthickness =  0)
 
-        Crapy.bouton_quit = Button(fenetre, text="quitter", command= fenetre.destroy)
+        Crapy.bouton_quit = Button(self.fenetre, text="quitter", command= self.fenetre.destroy)
 
         Crapy.debut()
 
@@ -78,10 +82,12 @@ class CrapsTk:
 
         #titre fenetre
 
-        fenetre.title("Craps")
+        self.fenetre.title("Craps")
 
         #fini
 
-        fenetre.mainloop()
+        self.fenetre.mainloop()
 
-CrapsTk.creation_premiere_fenetre()
+Crapy = CrapsTk()
+
+Crapy.creation_premiere_fenetre()
--- jeu.py
+++ jeu.py
@@ -12,6 +12,7 @@ et pour le côté mise - gains que nous trouvons attractif !"""
 
 from random import randint
 from tkinter import *
+from tkinter.messagebox import *
 
 class craps:
     def __init__(self, p):
@@ -31,26 +32,32 @@ class craps:
         self.cible = 0
         self.joue = True
 
-        self.IntroLab = None
         self.imagePhrase = None
         self.imphrase = None
         self.canimgphrase = None
+        self.bouton_popup_infos = None
+        self.labelportefeuille = None
+        self.labelentrypari = None
+        self.entrypari = None
 
     def debut_jeu(self):
 
-        self.IntroLab.grid(row=1, column=1, columnspan=2)
         self.imphrase = self.canimgphrase.create_image(340, 55, image=self.imagePhrase)
-        self.canimgphrase.grid(row=0, columnspan = 4)
+        self.canimgphrase.grid(row = 0, columnspan = 2)
+        self.bouton_popup_infos.place(x = 0, y = 0)
+        self.labelportefeuille.grid(row = 1, columnspan = 2)
+        self.labelentrypari.grid(row = 2, columnspan = 1)
+        self.entrypari.grid(row = 2, columnspan = 2)
 
-    def affiche_regles(self):
-        print("""Le Craps est un jeu de dés, que nous avons légèrement modifié dans le cadre de ce projet.
+    def get_regles(self):
+        return """Le Craps est un jeu de dés, que nous avons légèrement modifié dans le cadre de ce projet.
         L'ordinateur lance les dés, si au premier lancé vous obtenez 7 ou 11, vous doublez votre mise. Si vous obtenez 2, 3 ou 12, vous perdez.
         Pour toute autre valeur, vous relancez les dés, jusqu'à obtenir la même valeur de nouveau, ce qui doublera votre mise.
         Mais si dans l'intervalle vous obtenez un 7, vous perdez la mise.
         En fonction de votre portefeuille, vous obtenez des informations sur les développeurs de ce jeu.
         Nous avons choisi un jeu avec mise car il est facilement améliorable (multijoueur, système d'achat avec les gains...),
         et pour le côté mise - gains que nous trouvons attractif !
-        """)
+        """
 
     def getJoue(self):
         return self.joue
@@ -121,8 +128,6 @@ class craps:
 
         jeu = craps(500)
 
-        jeu.affiche_regles()
-
         while jeu.enCours():
             jeu.debut()
             jeu.miser()
@@ -132,6 +137,22 @@ class craps:
         print("\nVous n'avez plus rien à miser ! C'est la fin !")
     
     @staticmethod
+    def popInfoRegles():
+
+        #Partie moche pour éviter d'avoir une référence... dsl.
+        temp = craps(0)
+        to_show = temp.get_regles()
+
+        popRegles = Toplevel()
+        popRegles.title("Règles")
+
+        IntroLab = Label(popRegles, text = to_show, background = "white")
+        IntroLab.pack(side = "top", fill = "x", pady = 10)
+        BoutonPopRegles = Button(popRegles, text = "Ok", command = popRegles.destroy)
+        BoutonPopRegles.pack()
+        popRegles.mainloop()
+
+    @staticmethod
     def voir_jeu():
 
         print("===============")
@@ -140,7 +161,7 @@ class craps:
 
         game = craps(500)
 
-        fenetreGame = Toplevel()
+        fenetreGame = Tk()
 
         fenetreGame.geometry("820x450")
         fenetreGame.configure(background="white")
@@ -153,15 +174,17 @@ class craps:
         positionDown = int(fenetreGame.winfo_screenheight()/3 - windowHeight/2)
         fenetreGame.geometry("+{}+{}".format(positionRight, positionDown))
 
-
         game.imagePhrase = PhotoImage(file="image/leJeuDuMorpion.png")
         game.canimgphrase = Canvas(fenetreGame, width=660, height=100, background ="white", highlightthickness =  0)
-            
-        game.IntroLab = Label(fenetreGame, text = game.regles, background = "white")
+        game.bouton_popup_infos = Button(fenetreGame, width=5, height=2, text = "?", command = lambda : craps.popInfoRegles())
+        game.labelportefeuille = Label(fenetreGame, text = game.portefeuille)
+        game.labelentrypari = Label(fenetreGame, text = "Voici votre portefeuille :")
+        game.entrypari = Entry(fenetreGame, text = "Entrez la valeur à miser ici !")
+
         #game.IntroLab.pack()
 
         game.debut_jeu()
-        
+
         #titre fenetreGame
 
         fenetreGame.title("jeu")
